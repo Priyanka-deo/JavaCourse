5963473ebbe40366cd1e39f9363fd99fc067f2cf
priya <priyankadeshpande034@gmail.com>
2020-07-24 21:49:34 +0530

    Java program for fullName

---
+++ src/com/piyu/exercise/Exercise8.java
@@ -0,0 +1,11 @@
+package com.piyu.exercise;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        String firstName = "Priyanka";
+         String lastName = "Deo";
+         String result = " Priyanka Deo ";
+        System.out.println(" Full name is Priyanka Deo ");
+
+    }
+}
