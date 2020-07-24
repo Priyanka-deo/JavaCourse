b095023ed679f6b188bd68ab0c0957b84ee3b55e
priya <priyankadeshpande034@gmail.com>
2020-07-24 07:07:07 +0530

    Java program for Multiplication

---
+++ src/com/piyu/exercise/Exercise3.java
@@ -0,0 +1,10 @@
+package com.piyu.exercise;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 40;
+        int result = a * b;
+        System.out.println("Multiplication of " + a + " and " + b + " is " + result);
+    }
+}
