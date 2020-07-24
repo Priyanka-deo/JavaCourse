c200d05cfa84e58dca0ff5ebafc9aa9263427eaa
priya <priyankadeshpande034@gmail.com>
2020-07-24 19:55:30 +0530

    Java code for Monthly salary

---
+++ src/com/piyu/exercise/Exercise7.java
@@ -0,0 +1,12 @@
+package com.piyu.exercise;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        int annualSalary = 960;
+        int year = 12;
+        int result = (960 / 12);
+        System.out.println("Employee earns rupay monthly is " + result);
+
+
+    }
+}
