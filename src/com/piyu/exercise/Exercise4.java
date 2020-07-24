c922f9cb6adc3a2a45d6f614c9103dd9501821f5
priya <priyankadeshpande034@gmail.com>
2020-07-24 07:12:37 +0530

    Java code for Division

---
+++ src/com/piyu/exercise/Exercise4.java
@@ -0,0 +1,10 @@
+package com.piyu.exercise;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 20;
+        int result = a / b;
+        System.out.println("Division of " + a + " and " + b + " is " + result);
+    }
+}
