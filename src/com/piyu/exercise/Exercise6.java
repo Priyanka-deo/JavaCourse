dd9e5e8a1962b0cc349c4cbea6631300fd338a38
priya <priyankadeshpande034@gmail.com>
2020-07-24 21:28:03 +0530

    Java code for percentage

--- src/com/piyu/exercise/Exercise6.java
+++ src/com/piyu/exercise/Exercise6.java
@@ -1,7 +1,11 @@
 package com.piyu.exercise;
 
 public class Exercise6 {
-    public static void main(String[] args) {
-
+    public static void  main(String[] args) {
+    float totalMarks = 500;
+    float scoredMarks = 450;
+    double percentage = ((scoredMarks / totalMarks) * 100);
+    float f  =  ((450 / 500 ) * 100 );
+        System.out.println("priyanka got " + percentage + " in java exam");
     }
 }
