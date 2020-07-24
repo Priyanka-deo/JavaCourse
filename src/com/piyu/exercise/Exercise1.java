113b7e97c4ff97aad7a8cb4acd89d039887eae9f
priya <priyankadeshpande034@gmail.com>
2020-07-24 06:21:01 +0530

    Innitial commit

---
+++ src/com/piyu/exercise/Exercise1.java
@@ -0,0 +1,11 @@
+package com.piyu.exercise;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int result = a + b;
+        System.out.println("Addition of " + a + " and " + b + " is " + result);
+        
+    }
+}
