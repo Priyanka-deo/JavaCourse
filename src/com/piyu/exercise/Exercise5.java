7086c5746168bfa70db5025522a362d394b1f83f
priya <priyankadeshpande034@gmail.com>
2020-07-24 18:06:06 +0530

    Java code for avereage

--- src/com/piyu/exercise/Exercise5.java
+++ src/com/piyu/exercise/Exercise5.java
@@ -6,8 +6,8 @@ public class Exercise5 {
         int b = 20;
         int c = 30;
         int d = 40;
-        int result = a + b + c + d / 4;
-        System.out.println("Addition of " + a + " and " + b + " and " + c  +" and " + d + " is " + result / 4);
+        int result = (a + b + c + d )/ 4;
+        System.out.println("Addition of " + a + " and " + b + " and " + c  +" and " + d + " is " + result);
 
     }
 }
