7e07203f69a8b20cd5bc5f8854f794345813059e
priya <priyankadeshpande034@gmail.com>
2020-07-26 22:12:28 +0530

    Edited programs of Exercise 6,7,8

--- src/com/piyu/exercise/Exercise7.java
+++ src/com/piyu/exercise/Exercise7.java
@@ -2,11 +2,9 @@ package com.piyu.exercise;
 
 public class Exercise7 {
     public static void main(String[] args) {
-        int annualSalary = 960;
-        int year = 12;
-        int result = (960 / 12);
+        float annualSalary = 960;
+        int numberOfMonths = 12;
+        float result = (annualSalary / numberOfMonths);
         System.out.println("Employee earns " + result + " rupay monthly salary");
-
-
     }
 }
--- src/com/piyu/exercise/Exercise8.java
+++ src/com/piyu/exercise/Exercise8.java
@@ -3,9 +3,8 @@ package com.piyu.exercise;
 public class Exercise8 {
     public static void main(String[] args) {
         String firstName = "Priyanka";
-         String lastName = "Deo";
-         String result = " Priyanka Deo ";
-        System.out.println(" Full name is Priyanka Deo ");
-
+        String lastName = "Deo";
+        String result = firstName + " " + lastName;
+        System.out.println("Full name is " + result);
     }
 }
