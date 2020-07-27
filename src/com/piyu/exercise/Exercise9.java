db2488ada36409e866fdda7cc4ccdcb676cf46c1
priya <priyankadeshpande034@gmail.com>
2020-07-27 09:12:40 +0530

    Program for total Milk profit

---
+++ src/com/piyu/exercise/Exercise9.java
@@ -0,0 +1,11 @@
+package com.piyu.exercise;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        float sellingPrice = 22;
+        float costPrice = 20;
+        float quantity = 10;
+        float totalProfit = ((sellingPrice - costPrice) * quantity);
+            System.out.println(" Ankush has sold " + quantity + " liters of milk and earned rs " + totalProfit);
+    }
+}
