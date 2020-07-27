b18d578b17aa77572f52ae7a5ed8d20a87126722
priya <priyankadeshpande034@gmail.com>
2020-07-27 22:17:51 +0530

    Program for Pre and post Increment and Decrement

---
+++ src/com/piyu/exercise/Exercise11.java
@@ -0,0 +1,13 @@
+package com.piyu.exercise;
+
+public class Exercise11 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+        System.out.println(a);
+    }
+}
