4febf206b58ff3d93fb6fdb80e3b0fd27f197272
priya <priyankadeshpande034@gmail.com>
2020-07-27 09:34:06 +0530

    Program for total Amount Priyanka owes

--- src/com/piyu/exercise/Exercise10.java
+++ src/com/piyu/exercise/Exercise10.java
@@ -2,10 +2,10 @@ package com.piyu.exercise;
 
 public class Exercise10 {
     public static void main(String[] args) {
-        float intialAmountBorrpwed = 5000;
+        float initialAmountBorrowed = 5000;
         float rateOfInterest = 10;
         float numberOfYears = 5;
-        float totalAmountOwed = intialAmountBorrpwed * (1 + rateOfInterest * numberOfYears);
+        float totalAmountOwed = initialAmountBorrowed * (1 + rateOfInterest * numberOfYears);
             System.out.println(" Priyanka has to payback " + totalAmountOwed + " rs in " + numberOfYears + " years ");
     }
 }
