17e8e51bfb8c152864738bb73f524b66364a5dc8
priya <priyankadeshpande034@gmail.com>
2020-07-28 23:24:46 +0530

    program for Even or odd

---
+++ src/com/piyu/exercise/Evenorodd.java
@@ -0,0 +1,12 @@
+package com.piyu.exercise;
+
+public class Evenorodd {
+    public static void main(String[] args) {
+        int no = 90;
+        if (no % 2 == 0) {
+            System.out.println(+ no + " is even ");
+        } else {
+            System.out.println("number is odd");
+        }
+    }
+}
