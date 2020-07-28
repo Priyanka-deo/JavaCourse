dff044405023b5cec252e5e2c23ccc244e5f29f3
priya <priyankadeshpande034@gmail.com>
2020-07-28 11:06:21 +0530

    Program for Military Exam

---
+++ src/com/piyu/exercise/Exercise14.java
@@ -0,0 +1,15 @@
+package com.piyu.exercise;
+
+import javax.swing.*;
+
+public class Exercise14 {
+
+    public static void main(String[] args) {
+        int height = 6;
+        int age = 30;
+        if (height > 5 && age > 30) ;
+        {
+            System.out.println("You can give exam");
+        }
+    }
+}
