4c1aefc9accbbebd2202c5bd8f331a7714be61e5
priya <priyankadeshpande034@gmail.com>
2020-07-28 19:56:45 +0530

    program for logical operator 4

---
+++ src/com/piyu/exercise/Exercise19.java
@@ -0,0 +1,20 @@
+package com.piyu.exercise;
+
+public class Exercise19 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        if ((a++ != 10) & (b++ != 10)) {
+        }
+        System.out.println(a + "  " + b);
+        {
+            int c = 10;
+            int d = 20;
+            if ((c++ != 10) & (d++ != 10)) {
+                System.out.println(c + "  " + d);
+            }
+        }
+    }
+
+}
+
