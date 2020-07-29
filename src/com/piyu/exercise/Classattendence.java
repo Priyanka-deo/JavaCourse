12b98a8e6f6914479f7b0e6e19c72b57bb0f39d0
priya <priyankadeshpande034@gmail.com>
2020-07-29 18:34:13 +0530

    program for Numberofdigits

---
+++ src/com/piyu/exercise/Classattendence.java
@@ -0,0 +1,16 @@
+package com.piyu.exercise;
+
+public class Classattendence {
+    public static void main(String[] args) {
+        int NumberOfClassesHeld = 40;
+        int NumberOfClassesAttended = 28;
+        int Attendence = 75;
+        if (Attendence <= 75) {
+            System.out.println(" You have 75% Attendence you can give exam ");
+        } else {
+            System.out.println("You do not have 75% Attendence you can not gove exam");
+        }
+
+
+    }
+}
--- src/com/piyu/exercise/Evenorodd.java
+++ src/com/piyu/exercise/Evenorodd.java
@@ -4,9 +4,9 @@ public class Evenorodd {
     public static void main(String[] args) {
         int no = 35;
         if (no % 2 == 0) {
-            System.out.println(+ no + " is even ");
+            System.out.println( no + " is even ");
         } else {
-            System.out.println(+ no +" is odd ");
+            System.out.println( no +" is odd ");
         }
     }
 }
