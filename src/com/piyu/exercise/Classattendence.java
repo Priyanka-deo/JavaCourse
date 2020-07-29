eb149715382d55ff1a36039794eca4f2f9cccec3
priya <priyankadeshpande034@gmail.com>
2020-07-29 19:05:45 +0530

    program for Attendence

--- src/com/piyu/exercise/Classattendence.java
+++ src/com/piyu/exercise/Classattendence.java
@@ -4,13 +4,11 @@ public class Classattendence {
     public static void main(String[] args) {
         int NumberOfClassesHeld = 40;
         int NumberOfClassesAttended = 28;
-        int Attendence = 75;
-        if (Attendence <= 75) {
+        int Attendence = ((NumberOfClassesAttended / NumberOfClassesHeld) * 100);
+        if (Attendence >= 75) {
             System.out.println(" You have 75% Attendence you can give exam ");
         } else {
-            System.out.println("You do not have 75% Attendence you can not gove exam");
+            System.out.println("You do not have 75% Attendence you can not give exam ");
         }
-
-
     }
 }
