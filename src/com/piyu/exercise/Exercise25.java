a7edffd374c7dfa8f3fb852af00c8257edd456f4
priya <priyankadeshpande034@gmail.com>
2020-07-30 10:55:55 +0530

    program for Explicittypecasting

---
+++ src/com/piyu/exercise/Exercise25.java
@@ -0,0 +1,10 @@
+package com.piyu.exercise;
+
+public class Exercise25 {
+    public static void main(String[] args) {
+        byte b1 = 30;
+        byte b2 = 30;
+        byte b = (byte)(b1+b2);
+        System.out.println(b);
+    }
+}
