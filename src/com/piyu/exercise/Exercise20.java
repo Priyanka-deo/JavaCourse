976d5657c970ae2006537f3c40211fc352b41214
priya <priyankadeshpande034@gmail.com>
2020-07-30 19:46:51 +0530

    program for out put -126

--- src/com/piyu/exercise/Exercise20.java
+++ src/com/piyu/exercise/Exercise20.java
@@ -2,6 +2,6 @@ package com.piyu.exercise;
 
 public class Exercise20 {
     public static void main(String[] args) {
-        System.out.println(Long.MIN_VALUE+"   "+ Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE+"   "+ Long.MAX_VALUE);//
     }
 }
