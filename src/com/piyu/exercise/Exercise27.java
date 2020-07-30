aa6084ad3dac3ed4cca2f0ff30575079eed5abdc
priya <priyankadeshpande034@gmail.com>
2020-07-30 11:02:04 +0530

    program for Implicittypecasting

---
+++ src/com/piyu/exercise/Exercise27.java
@@ -0,0 +1,9 @@
+package com.piyu.exercise;
+
+public class Exercise27 {
+    public static void main(String[] args) {
+        int i = 130;
+        byte b = (byte) i;
+        System.out.println(b);
+    }
+}
