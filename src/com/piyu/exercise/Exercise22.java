ab01774f053e984f9f0e5fb81f94168416dc5945
priya <priyankadeshpande034@gmail.com>
2020-07-30 10:11:17 +0530

    program for int to byte

---
+++ src/com/piyu/exercise/Exercise22.java
@@ -0,0 +1,11 @@
+package com.piyu.exercise;
+
+public class Exercise22 {
+
+    public static void main(String[] args) {
+        byte b1 = 30;
+        byte b2 = 30;
+        byte b = b1 + b2;
+    }
+}
+
