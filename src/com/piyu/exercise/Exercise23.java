91c632443ad80434a8439348fd8b5bcc22dee768
priya <priyankadeshpande034@gmail.com>
2020-07-30 10:38:32 +0530

    program for Explicittypecasting

---
+++ src/com/piyu/exercise/Exercise23.java
@@ -0,0 +1,9 @@
+package com.piyu.exercise;
+
+public class Exercise23 {
+    public static void main(String[] args) {
+        long l = 10;
+        float f = l;
+        System.out.println(l + "  " + f);
+    }
+}
---
+++ src/com/piyu/exercise/Exercise24.java
@@ -0,0 +1,9 @@
+package com.piyu.exercise;
+
+public class Exercise24 {
+    public static void main(String[] args) {
+        int i = 10;
+        short s = (byte)i;
+        System.out.println(i + "  "+s);
+    }
+}
