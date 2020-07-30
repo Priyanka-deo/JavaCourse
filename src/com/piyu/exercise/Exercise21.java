ec3e0f9afddcdfe654d60683ac7fde7fe4cfc901
priya <priyankadeshpande034@gmail.com>
2020-07-30 09:50:48 +0530

    program for chartoint

---
+++ src/com/piyu/exercise/Exercise21.java
@@ -0,0 +1,9 @@
+package com.piyu.exercise;
+
+public class Exercise21 {
+    public static void main(String[] args) {
+        char c = 'A';
+        int i = c;
+        System.out.println(c + "  " + i);
+    }
+}
