a81aa5196fc47a11c803be6e7c0a696208521da5
priya <priyankadeshpande034@gmail.com>
2020-07-30 19:18:41 +0530

    program for Implicittypecasting

--- src/com/piyu/exercise/Exercise27.java
+++ src/com/piyu/exercise/Exercise27.java
@@ -4,6 +4,6 @@ public class Exercise27 {
     public static void main(String[] args) {
         int i = 130;
         byte b = (byte) i;
-        System.out.println(b);
+        System.out.println(b);// byte is left side datatype i.e higher than int 
     }
 }
