4a8eb0a3db8abfaf74b168e1a7f02201308c7139
priya <priyankadeshpande034@gmail.com>
2020-07-30 09:30:04 +0530

    program for Bonus

--- src/com/piyu/exercise/EmpBonus.java
+++ src/com/piyu/exercise/EmpBonus.java
@@ -2,15 +2,17 @@ package com.piyu.exercise;
 
 public class EmpBonus {
     public static void main(String[] args) {
-        float yearsOfService = 5.5f;
+        float yearsOfService = 10.5f;
         double salary = 8999.99;
-        double bonus = (salary * 5) / 100;
+        double bonus = ((salary * 5) / 100);
         if (yearsOfService >= 5 && yearsOfService < 10) {
+            bonus = (salary * 5) / 100;
             System.out.println("your year Of Service is more than 5 will get " + bonus);
         } else if (yearsOfService >= 10) {
+            bonus = (salary * 10) / 100;
             System.out.println("your year Of Service is more than 10 will get " + bonus);
         } else {
-            System.out.println(" No bonus ");
+            System.out.println("There is No bonus for you ");
         }
     }
 }
