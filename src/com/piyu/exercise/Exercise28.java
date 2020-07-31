48cc1b3982c142f02743f2b66ce00e02075a73f8
priya <priyankadeshpande034@gmail.com>
2020-07-31 08:52:55 +0530

    program28

---
+++ src/com/piyu/exercise/Exercise28.java
@@ -0,0 +1,9 @@
+package com.piyu.exercise;
+
+public class Exercise28 {
+    public static void main(String[] args) {
+        byte b = -25;
+        byte i = b++;
+        System.out.println(+b + "  " + i);
+    }
+}
