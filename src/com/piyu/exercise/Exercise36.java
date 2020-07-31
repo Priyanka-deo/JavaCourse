2a68927d36ebf7b1df2586d8297c3b48b089c287
priya <priyankadeshpande034@gmail.com>
2020-07-31 21:18:20 +0530

    program36

---
+++ src/com/piyu/exercise/Exercise36.java
@@ -0,0 +1,27 @@
+package com.piyu.exercise;
+
+public class Exercise36 {
+    public static void main(String[] args) {
+        char c = 'B';
+        switch (c) {
+            case 'A':
+                System.out.println("Five");
+                break;
+            case 'B':
+                System.out.println("Ten");
+                break;
+            case 'C':
+                System.out.println("Fifteen");
+                break;
+            case 'D':
+                System.out.println("Twenty");
+                break;
+            default:
+                System.out.println("default");
+                break;
+
+
+        }
+
+    }
+}
