09e1959a96c0c81c2b6aa4756204f28e99af7f8f
priya <priyankadeshpande034@gmail.com>
2020-07-31 20:20:58 +0530

    program33

--- src/com/piyu/exercise/Exercise33.java
+++ src/com/piyu/exercise/Exercise33.java
@@ -1,6 +1,6 @@
 package com.piyu.exercise;
 
-public class Exercise32 {
+public class Exercise33 {
     public static void main(String[] args) {
         int i = 10;
         int j;
@@ -8,11 +8,9 @@ public class Exercise32 {
             j = 20;
         } else if (i == 20) {
             j = 30;
-        }
-        else
-        {
+        } else {
             j = 40;
         }
-        System.out.println(j);// j variable is not initialize
+        System.out.println(j);
     }
 }
