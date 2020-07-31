4fd37483d4143da8e6eb83fe865a4374a0634f6d
priya <priyankadeshpande034@gmail.com>
2020-07-31 21:36:25 +0530

    program37

---
+++ src/com/piyu/exercise/Exercise37.java
@@ -0,0 +1,10 @@
+package com.piyu.exercise;
+
+public class Exercise37 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) ;
+        {
+            System.out.println(i);
+        }
+    }
+}
