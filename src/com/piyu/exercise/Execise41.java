db605bb4f88046112f8d3ca852c77798548da93f
priya <priyankadeshpande034@gmail.com>
2020-08-01 08:18:59 +0530

    program41

--- src/com/piyu/exercise/Execise41.java
+++ src/com/piyu/exercise/Execise41.java
@@ -2,10 +2,9 @@ package com.piyu.exercise;
 
 public class Execise41 {
     public static void main(String[] args) {
-
-        for(int i = 0;float f = 0f;i < 10 && f < 10.0f;i++,f++)
+        for(int i = 0; j = 0; i < 10; && j < 10; i++;j++)
         {
-            System.out.println(i+ "  "+f);
+            System.out.println(i+ "  "+j);
 
         }
     }
