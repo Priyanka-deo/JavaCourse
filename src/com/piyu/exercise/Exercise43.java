3d35fd4d231a4e04b2119760fa974fcd5b4f1224
priya <priyankadeshpande034@gmail.com>
2020-08-01 08:42:37 +0530

    program43

---
+++ src/com/piyu/exercise/Exercise43.java
@@ -0,0 +1,10 @@
+package com.piyu.exercise;
+
+public class Exercise43 {
+    public static void main(String[] args) {
+        int no;
+        for (no = 2; no <= 100; no = (no + 2)) {
+            System.out.println(no);
+        }
+    }
+}
