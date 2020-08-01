a3a721533ba4a5d75829f79571d6628368ead52d
priya <priyankadeshpande034@gmail.com>
2020-08-01 08:11:36 +0530

    program41

--- src/com/piyu/exercise/Execise41.java
+++ src/com/piyu/exercise/Execise41.java
@@ -2,8 +2,8 @@ package com.piyu.exercise;
 
 public class Execise41 {
     public static void main(String[] args) {
-        
-        for(int i = 0;float f = 0f;i < 10 && f < 10.0f;i++;f++)
+
+        for(int i = 0;float f = 0f;i < 10 && f < 10.0f;i++,f++)
         {
             System.out.println(i+ "  "+f);
 
---
+++ src/com/piyu/exercise/Exercise42.java
@@ -0,0 +1,10 @@
+package com.piyu.exercise;
+
+public class Exercise42 {
+    public static void main(String[] args) {
+            for (int i = 0, j = 0; i < 10 && j < 10; i++, j++)
+            {
+                System.out.println(i + "   " + j);
+            }
+        }
+    }
