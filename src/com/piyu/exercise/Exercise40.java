b8d5e37874fd41363a5b5a4a2ae66855c03d0b20
priya <priyankadeshpande034@gmail.com>
2020-08-01 07:57:19 +0530

    program40

---
+++ src/com/piyu/exercise/Exercise40.java
@@ -0,0 +1,11 @@
+package com.piyu.exercise;
+
+public class Exercise40 {
+    public static void main(String[] args) {
+        int i = 0;
+        for(System.out.println("Hellow");i < 10; i ++)
+        {
+            System.out.println(i);
+        }
+    }
+}
