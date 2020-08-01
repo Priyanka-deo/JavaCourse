3745c6f6fcb5e1b69a5ccb42724bfe9d2cde43f7
priya <priyankadeshpande034@gmail.com>
2020-08-01 19:08:15 +0530

    program46

---
+++ src/com/piyu/exercise/Exercise45.java
@@ -0,0 +1,12 @@
+package com.piyu.exercise;
+
+public class Exercise45 {
+    public static void main(String[] args) {
+        System.out.println("Before loop");
+        for(i = 0;true;i++)
+        {
+            System.out.println("Inside loop");
+        }
+        System.out.println("Outside loop");
+    }
+}
---
+++ src/com/piyu/exercise/Exercise46.java
@@ -0,0 +1,12 @@
+package com.piyu.exercise;
+
+public class Exercise46 {
+    public static void main(String[] args) {
+        System.out.println("Before loop");
+        for(int i = 0;;i++)
+        {
+            System.out.println("Inside loop");
+        }
+        System.out.println("After loop");
+    }
+}
