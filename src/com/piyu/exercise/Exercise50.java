c9778407c7c2a290e64e87b1dbd5c506bbd6f8ab
priya <priyankadeshpande034@gmail.com>
2020-08-02 18:40:00 +0530

    program50

---
+++ src/com/piyu/exercise/Exercise50.java
@@ -0,0 +1,15 @@
+package com.piyu.exercise;
+
+public class Exercise50 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i <= 10);
+    }
+}
+
+
+
