6368c8148ac7a750f50a27d02ff1dcfa00948111
priya <priyankadeshpande034@gmail.com>
2020-08-03 07:41:58 +0530

    program53

---
+++ src/com/piyu/exercise/Exercise53.java
@@ -0,0 +1,15 @@
+package com.piyu.exercise;
+
+public class Exercise53 {
+    public static void main(String[] args) {
+        System.out.println("Before loop");
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                System.out.println("Inside loop Before continue");
+                continue;
+                System.out.println("Inside loop After continue");
+            }
+        }
+        System.out.println("After loop");
+    }
+}
