022b6188d3f2798526cbf7d1bf731ff82ce85380
priya <priyankadeshpande034@gmail.com>
2020-08-06 18:43:00 +0530

    program for multiplication of number

---
+++ src/com/piyu/exercise/Exercise56.java
@@ -0,0 +1,10 @@
+package com.piyu.exercise;
+
+public class Exercise56 {
+    public static void main(String[] args) {
+        int no = 20;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(no + " * " + i + " = " + no * i);
+        }
+    }
+}
