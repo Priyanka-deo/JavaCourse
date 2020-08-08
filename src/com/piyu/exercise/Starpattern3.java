65a65cd9fe2fdae1119b723240b674bdf71e99a9
priya <priyankadeshpande034@gmail.com>
2020-08-08 19:14:46 +0530

    program Starpattern3

--- src/com/piyu/exercise/Starpattern3.java
+++ src/com/piyu/exercise/Starpattern3.java
@@ -9,7 +9,7 @@ public class Starpattern3 {
             System.out.println(" ");
         }
         for (int i = 1; i <= 4; i++) {
-            for (int j = 4; j >= i; j--) {
+            for (int j = 3; j >= i; j--) {
                 System.out.print("*");
             }
             System.out.println(" ");
