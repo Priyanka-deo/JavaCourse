468fb1ee5140158fae3b5dd8d205c86bcaee94c5
priya <priyankadeshpande034@gmail.com>
2020-08-08 18:37:14 +0530

    program Starpattern1

--- src/com/piyu/exercise/PrimeNo.java
+++ src/com/piyu/exercise/PrimeNo.java
@@ -4,16 +4,18 @@ public class PrimeNo {
     public static void main(String[] args) {
         int no = 7;
         int temp = 0;
+        boolean flag = false;
         for (int i = 2; i <= no - 1; i++) {
             if (no % i == 0) ;
             {
-                temp = temp + 1;
+               flag = true;
+               break;
             }
         }
-        if (temp == 0) {
-            System.out.println(no + " is  not prime number ");
+        if (flag) {
+            System.out.println(no + " is  prime number ");
         } else {
-            System.out.println(no + " is prime number ");
+            System.out.println(no + " is not prime number ");
         }
     }
 }
