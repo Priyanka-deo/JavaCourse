a59f613af61947a340178b5335095df0b8896a24
priya <priyankadeshpande034@gmail.com>
2020-08-10 20:39:08 +0530

    program Starpattern6

---
+++ src/com/piyu/exercise/Starpattern6.java
@@ -0,0 +1,14 @@
+package com.piyu.exercise;
+
+public class Starpattern6 {
+    public static void main(String[] args){
+        for(int i =1;i <= 4;i++)
+        {
+            for(int j=1;j<=8;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
