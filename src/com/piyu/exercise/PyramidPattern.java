34ffc4018b7494fcd1da258bd99916d74a8e4a93
priya <priyankadeshpande034@gmail.com>
2020-08-11 10:40:40 +0530

    program PyramidPattern

--- src/com/piyu/exercise/PyramidPattern.java
+++ src/com/piyu/exercise/PyramidPattern.java
@@ -1,16 +1,15 @@
 package com.piyu.exercise;
 
-public class Starpattern7 {
+public class PyramidPattern {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j >= i; j++) {
-                System.out.print("*");
+            System.out.println();
+            for (int j = 1; j <= 5-i; j++) {
+                System.out.print("  ");
             }
             for (int k = 1; k <= i; k++) {
-                System.out.print("*");
+                System.out.print("  *  ");
             }
-            System.out.println("*");
-
         }
     }
 }
--- src/com/piyu/exercise/Staepattern7.java
+++
@@ -1,5 +0,0 @@
-package com.piyu.exercise;
-
-public class Staepattern7 {
-
-}
