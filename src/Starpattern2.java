e1423842be17e77308482a5a735e7f7048ce8017
priya <priyankadeshpande034@gmail.com>
2020-08-12 00:21:01 +0530

    program PyramidPattern2

--- src/Starpattern2.java
+++ src/Starpattern2.java
@@ -1,7 +1,9 @@
 public class Starpattern2 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
+        int NumberOfVerticalLines = 5;
+        int NumberOfHorizontalLines = 5;
+        for (int i = 1; i <= NumberOfHorizontalLines; i++) {
+            for (int j = 1; j<=i * 1; j++) {
                 System.out.print("*");
             }
             System.out.println(" ");
