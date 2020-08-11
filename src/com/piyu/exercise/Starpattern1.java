f150a102629df56446bbf6578496ab1998cfce5a
priya <priyankadeshpande034@gmail.com>
2020-08-12 00:06:06 +0530

    program PyramidPattern3

--- src/com/piyu/exercise/Starpattern1.java
+++ src/com/piyu/exercise/Starpattern1.java
@@ -3,7 +3,7 @@ package com.piyu.exercise;
 public class Starpattern1 {
     public static void main(String[] args) {
         int NumberofVerticalLines = 4;
-        int NumberOfHorizontalLines = 5;
+        int NumberOfHorizontalLines = 8;
         for (int i = 1; i <= NumberofVerticalLines; i++) {
             for (int j = 1; j <= NumberOfHorizontalLines; j++) {
                 System.out.print("*");
--- src/com/piyu/exercise/Starpattern3.java
+++ src/com/piyu/exercise/Starpattern3.java
@@ -2,14 +2,13 @@ package com.piyu.exercise;
 
 public class Starpattern3 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+       int NumberOfVerticalLines = 5;
+       int NumberOfHorizontalLines = 5;
+        for (int i = 1; i <= NumberOfHorizontalLines; i++) {
+            for (int j = NumberOfVerticalLines - 1; j >= i; j--) {
+                System.out.print(" ");
             }
-            System.out.println(" ");
-        }
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 3; j >= i; j--) {
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println(" ");
