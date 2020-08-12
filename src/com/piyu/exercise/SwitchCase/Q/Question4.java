da9fed442f629e84dd842a02c8df91ec658b06d5
priya <priyankadeshpande034@gmail.com>
2020-08-12 22:02:51 +0530

    Question4

---
+++ src/com/piyu/exercise/SwitchCase/Q/Question4.java
@@ -0,0 +1,22 @@
+package com.piyu.exercise.SwitchCase.Q;
+
+public class Question4 {
+    public static void main(String[] s) {
+        char direction = 'N';
+        char west = 'W';
+        switch (direction)
+        {
+            case 'N':
+                System.out.println("North");
+                break;
+            case 'E':
+                System.out.println("East");
+                break;
+            case west:
+                System.out.println("West");
+                break;
+            case 'S':
+                System.out.println("South");
+        }
+    }
+}
