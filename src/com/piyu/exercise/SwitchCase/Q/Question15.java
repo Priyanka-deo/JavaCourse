b57f5b7cf9daba0073dca2a41ef5844ee17cbd9d
priya <priyankadeshpande034@gmail.com>
2020-08-13 07:36:58 +0530

    Question15

---
+++ src/com/piyu/exercise/SwitchCase/Q/Question15.java
@@ -0,0 +1,41 @@
+package com.piyu.exercise.SwitchCase.Q;
+
+public class Question15 {
+    public static void main(String[] args) {
+        String lion = "lion";
+        String deer = "Deer";
+        String donkey = "Donkey";
+        String monkey = "Monkey";
+        String allAnimals = "All Animal";
+        int number = 0;
+        switch (number){
+            case 0:
+                System.out.println(allAnimals + "are :");
+            case 1:
+                int i = 1;
+                System.out.println(i + " . " + lion);
+                if (number != 0){
+                    break;
+                }
+            case 2:
+              int i = 2;
+                System.out.println(i + "." + deer);
+                if(number != 0)
+                {
+                    break;
+                }
+            case 3:
+                int i = 3;
+                System.out.println(i + "."+ donkey);
+                if(number != 0)
+                {
+                    break;
+                }
+            case 4:
+               System.out.println(i + "." + monkey);
+                break;
+            default:
+                System.out.println("No Animals are present");
+        }
+    }
+}
