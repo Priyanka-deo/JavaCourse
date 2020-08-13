a5cec6d3d9405530bcf7933294d0851e0370a9b7
priya <priyankadeshpande034@gmail.com>
2020-08-13 07:55:51 +0530

    Question18

---
+++ src/com/piyu/exercise/SwitchCase/Q/Question18.java
@@ -0,0 +1,20 @@
+package com.piyu.exercise.SwitchCase.Q;
+
+public class Question18 {
+    public static void main(String[] args) {
+        int x = 111, y = 101;
+        switch (x & y)
+        {
+            case 1:
+                System.out.println("-1-");
+            case 101:
+                System.out.println("-101-");
+            case 111:
+                System.out.println("-111-");
+            case 010:
+                System.out.println("-010-");
+            default:
+                System.out.println("-" + x & y + "-");
+        }
+    }
+}
