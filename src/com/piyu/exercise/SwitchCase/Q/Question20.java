d233b838d4d9495a8f307c6c29b72503977a2f9d
priya <priyankadeshpande034@gmail.com>
2020-08-14 17:40:02 +0530

    Question20

--- src/com/piyu/exercise/SwitchCase/Q/Question20.java
+++ src/com/piyu/exercise/SwitchCase/Q/Question20.java
@@ -2,7 +2,7 @@ package com.piyu.exercise.SwitchCase.Q;
 
 public class Question20 {
     public static void main(String[] args) {
-        for(int i = 0; String = 'GFG';i < 2; i++)
+       // for(int i = 0; String = 'GFG';i < 2; i++)
         System.out.println("HELLO GEEKS");
     }
 }
