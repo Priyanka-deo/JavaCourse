6e9bb0d7450647ba4d99fa67e00aba46ed7b39f6
priya <priyankadeshpande034@gmail.com>
2020-08-14 20:15:54 +0530

    Question26

--- src/com/piyu/exercise/SwitchCase/Q/Question26.java
+++ src/com/piyu/exercise/SwitchCase/Q/Question26.java
@@ -1,12 +1,12 @@
 package com.piyu.exercise.SwitchCase.Q;
 
-public class Question25 {
+public class Question26 {
     public static void main(String[] args) {
         int numberOfVerticalLines = 5;
         int numberOfHorizontalLines = 5;
         for (int i = 1; i <= numberOfHorizontalLines; i++) {
-            for (int j = 1; j <= i ; j++) {
-                System.out.print(j);
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
             }
             System.out.println();
         }
