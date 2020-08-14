b9fb4100478b59f457bae0245f1afeeba9f6c4a8
priya <priyankadeshpande034@gmail.com>
2020-08-14 17:46:35 +0530

    Question22

---
+++ src/com/piyu/exercise/SwitchCase/Q/Question21.java
@@ -0,0 +1,9 @@
+package com.piyu.exercise.SwitchCase.Q;
+
+public class Question21 {
+    public static void main(String[] args) {
+        int i = 0;
+        for(System.out.println("HI");i < 1; i++);
+        System.out.println("HELLO GEEKS");
+    }
+}
---
+++ src/com/piyu/exercise/SwitchCase/Q/Question22.java
@@ -0,0 +1,8 @@
+package com.piyu.exercise.SwitchCase.Q;
+
+public class Question22 {
+    public static void main(String[] args) {
+        for(int i = 0; 1 < 1; System.out.println("WELCOME") )
+            //System.out.println("WELCOME GEEKS");
+        }
+}
