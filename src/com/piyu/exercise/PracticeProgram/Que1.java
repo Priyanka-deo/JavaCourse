9ba68f63a91dc7e139c099b0b2cba597c83df57b
priya <priyankadeshpande034@gmail.com>
2020-09-27 23:02:56 +0530

    Que1,Que2,Que3

---
+++ src/com/piyu/exercise/PracticeProgram/Que1.java
@@ -0,0 +1,14 @@
+package com.piyu.exercise.PracticeProgram;
+
+public class Que1 {
+    public static void main(String[] args) {
+        int i = 10;
+        int j;
+        if (i == 10) {
+            j = 20;
+        } else {
+            j = 30;
+        }
+        System.out.println(j);
+    }
+}
---
+++ src/com/piyu/exercise/PracticeProgram/Que3.java
@@ -0,0 +1,12 @@
+package com.piyu.exercise.PracticeProgram;
+
+public class Que3 {
+    public static void main(String[] args) {
+        final int i = 10;
+        int j;
+        if(i == 10){
+            j = 20;
+        }
+        System.out.println(j);
+    }
+}
