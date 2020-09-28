ecccf863e942722ec75cf274121aac6a98de443f
priya <priyankadeshpande034@gmail.com>
2020-09-28 17:42:37 +0530

    TransferStatements,WhileLoop

---
+++ src/com/piyu/exercise/PracticeProgram/WhileLoop/Que3.java
@@ -0,0 +1,13 @@
+package com.piyu.exercise.PracticeProgram.WhileLoop;
+
+public class Que3 {
+    public static void main(String[] args) {
+        System.out.println("Before Loop");
+        do {
+            System.out.println("Inside Loop");
+        }
+        while (true);
+       // System.out.println("After Loop");
+    }
+
+}
