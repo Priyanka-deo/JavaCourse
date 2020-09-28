781b5bfaf21bb8afb15f17218b45b6234b2f4fba
priya <priyankadeshpande034@gmail.com>
2020-09-28 17:05:23 +0530

    Que12

---
+++ src/com/piyu/exercise/PracticeProgram/Que12.java
@@ -0,0 +1,11 @@
+package com.piyu.exercise.PracticeProgram;
+
+public class Que12 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; System.out.println("Hello"))
+        {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+}
